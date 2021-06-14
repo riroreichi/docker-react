d53b9d2b755fac91ff109e27eedb63bba0b86cfc
riroreichi <riroreichi@gmail.com>
2021-06-14 12:16:37 +0300

    redeploy to aws

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello I here to test
+          Initial React app
         </p>
+        <p>New Text added</p>
         <a
           className="App-link"
           href="https://reactjs.org"
