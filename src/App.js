ef80340c9c3af33d48a68826b723fc14ecac51d6
riroreichi <riroreichi@gmail.com>
2021-06-14 11:44:17 +0300

    Restored original app.js but added changed Welcome text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello I here to test the test and test again
+          Initial React app
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
